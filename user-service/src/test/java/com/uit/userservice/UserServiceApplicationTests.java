11f340db6249a431a8699bb2794696fbf542e176
Phu Tho <phanphutho9999@gmail.com>
2025-10-20 19:58:26 +0700

    fix: correct MapStruct configuration and disable config client in tests

--- user-service/src/test/java/com/uit/userservice/UserServiceApplicationTests.java
+++ user-service/src/test/java/com/uit/userservice/UserServiceApplicationTests.java
@@ -3,7 +3,7 @@ package com.uit.userservice;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = {"spring.cloud.config.enabled=false"})
 class UserServiceApplicationTests {
 
     @Test
