e4ef2c845a74608109c061569aa5e102a86ba080
Phu Tho <phanphutho9999@gmail.com>
2025-10-22 11:36:52 +0700

    feat: Configure account-service with Keycloak and database

---
+++ account-service/src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO accounts (account_id, user_id, balance, currency, type) VALUES
+('ACC001', 'a97acebd-b885-4dcd-9881-c9b2ef66e0ea', 1000000.00, 'USD', 'CHECKING'),
+('ACC002', 'user-jane-doe', 12345.00, 'USD', 'SAVINGS'),
+('ACC003', 'user-john-smith', 987.65, 'USD', 'CHECKING');
