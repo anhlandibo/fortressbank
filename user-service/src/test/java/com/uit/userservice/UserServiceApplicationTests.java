a00bc46f7c5b5f57fc59d663dcbaaa71327c7ae4
Phu Tho <phanphutho9999@gmail.com>
2025-10-22 11:37:00 +0700

    feat: Integrate user-service with Keycloak and update configurations

--- user-service/src/test/java/com/uit/userservice/UserServiceApplicationTests.java
+++ user-service/src/test/java/com/uit/userservice/UserServiceApplicationTests.java
@@ -3,11 +3,14 @@ package com.uit.userservice;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest(properties = {"spring.cloud.config.enabled=false"})
+@SpringBootTest(properties = {
+    "spring.cloud.config.enabled=false",
+    "spring.cloud.config.import-check.enabled=false"
+})
 class UserServiceApplicationTests {
 
     @Test
     void contextLoads() {
+        // Context should load successfully
     }
-
 }
