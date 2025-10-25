a691ee936e4076f28f8707cf96549b4bad6bfa85
Phu Tho <phanphutho9999@gmail.com>
2025-10-25 13:22:19 +0700

    feat: Update shared-kernel, notification-service, and risk-engine

--- notification-service/src/main/java/com/uit/notificationservice/dto/SendSmsOtpRequest.java
+++ notification-service/src/main/java/com/uit/notificationservice/dto/SendSmsOtpRequest.java
@@ -1,8 +1,12 @@
 package com.uit.notificationservice.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class SendSmsOtpRequest {
     private String phoneNumber;
     private String otpCode;
