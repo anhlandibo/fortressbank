fc7e6863a8cbf4a1b5acd1bc57f46ad7727b64f8
Phu Tho <phanphutho9999@gmail.com>
2025-10-26 12:21:42 +0700

    feat(account-service): Configure Redis for proper object serialization and externalize host

--- account-service/src/main/java/com/uit/accountservice/riskengine/dto/RiskAssessmentRequest.java
+++ account-service/src/main/java/com/uit/accountservice/riskengine/dto/RiskAssessmentRequest.java
@@ -2,11 +2,13 @@ package com.uit.accountservice.riskengine.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class RiskAssessmentRequest {
     private BigDecimal amount;
     private String userId;
